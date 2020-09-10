e5621f49322d2a88ae9b77ca9902689a88f91886
Barros Edijanio <barrosed@gi-de.com>
2020-09-10 20:44:09 -0300

    first commit

---
+++ src/main/java/br/com/api/model/Category.java
@@ -0,0 +1,5 @@
+package br.com.api.model;
+
+public class Category {
+
+}
---
+++ src/test/java/br/com/api/houseparty/HousepartyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.houseparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousepartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
