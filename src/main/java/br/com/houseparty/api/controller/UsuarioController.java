3d136006d85534eaadb38cef2bd2c09f674c9413
edijanio <edijanio.souza@hotmail.com>
2020-09-25 15:22:13 -0300

    criado o usuarioRepositorio

--- src/main/java/br/com/houseparty/api/controller/UsuarioController.java
+++ src/main/java/br/com/houseparty/api/controller/UsuarioController.java
@@ -3,23 +3,18 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.houseparty.api.model.Cliente;
 import br.com.houseparty.api.model.Usuario;
+import br.com.houseparty.api.repositorios.UsuarioRepositorio;
+
 
 @RestController
+@RequestMapping({"/usuario"})
 public class UsuarioController {
-
 	
-	@GetMapping("/usuario/cliente")
-	List<Cliente> getCliente(){
-		
-	}
-	
-	@PostMapping("/usuario/cliente")
-	Cliente postCliente(@RequestBody Usuario cliente) {
-		
-	}
+	private UsuarioRepositorio usuarioRepositorio;
 	
 }
